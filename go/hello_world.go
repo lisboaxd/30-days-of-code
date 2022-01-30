6083de0297678755a9c4d67d4edbd92f102728c8
Mateus Lisboa <mateuslnobre@gmail.com>
2022-01-30 18:52:06 -0300

    Modify code for the hackerRank problem requiriments

--- go/hello_world.go
+++ go/hello_world.go
@@ -1,7 +1,17 @@
 package main
-
-import "fmt"
+import (
+    "fmt"
+    "strings"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    var inputString [6]string
+    fmt.Scan(&inputString[0])
+    fmt.Scan(&inputString[1])
+    fmt.Scan(&inputString[2])
+    fmt.Scan(&inputString[3])
+    fmt.Scan(&inputString[4])
+    fmt.Scan(&inputString[5])
+    fmt.Println("Hello, World.")
+    fmt.Println(strings.Join(inputString[:], " "))
 }
